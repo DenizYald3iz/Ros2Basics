b021a6574916f8eec977b730e00195c8eac6fce5
Deniz YALDIZ <54864981+DenizYald3iz@users.noreply.github.com>
2023-10-05 09:27:23 +0300

    Add files via upload

---
+++ draw_circle.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+import rclpy
+from rclpy.node import Node
+from geometry_msgs.msg import Twist
+
+class DrawCircleNode(Node):
+    def __init__(self):
+        super().__init__("draw_circle")
+        self.cmd_vel_pub_ = self.create_publisher(Twist, "/turtle1/cmd_vel", 10)
+        self.timer_ = self.create_timer(0.5, self.send_velocity_command)#calls the function send_velocity_command ever 0.5 second
+        self.get_logger().info("Draw circle node has been started")
+
+    def send_velocity_command(self):
+        msg = Twist()
+        msg.linear.x = 2.0
+        msg.angular.z = 0.5
+        self.cmd_vel_pub_.publish(msg)
+
+
+def main(args= None):
+    rclpy.init(args=args)
+
+    node = DrawCircleNode()
+    rclpy.spin(node)
+
+
+
+    rclpy.shutdown()
